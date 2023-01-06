e8b3e423bd3e58a9696383f0b097d37da90161e5
Joel_Paiva <joelpaiva112233@gmail.com>
2023-01-06 16:39:42 +0200

    still learning python oop

--- pyoop/main.py
+++ pyoop/main.py
@@ -1,10 +1,14 @@
 #!/bin/python3
 
 class students:
+    numofstudents = 0
     def __init__(self,  fname, lname, email):
         self.fname = fname
         self.lname = lname
         self.email = email
+
+        students.numofstudents += 1
+
     def fullname(self):
         print(self.fname + ' ' + self.lname)
 
@@ -14,5 +18,7 @@ class students:
 stu1 = students('joyien', 'noscope', "jojonoscope22@gmail.com")
 stu2 = students('DERTY', 'boi', "dertiboi8080@gmail.com")
 
-stu1.fullname()
+print(students.numofstudents)
+
+students.fullname(stu1)
 
---
+++ sockpy/heresock.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+
+import socket
+import os
+
+ipaddr = input("Enter the ip address to connect to: ")
+port = input
