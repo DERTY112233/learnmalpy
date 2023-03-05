377e9e3ef3492b28f00496af5182e27bd61524e1
DERTY112233 <joelpaiva112233@gmail.com>
2023-03-05 12:53:37 +0200

    added multiple downloads function to youdown

--- automate/youdown.py
+++ automate/youdown.py
@@ -1,10 +1,19 @@
 import youtube_dl
 import pyfiglet
+import subprocess
+import os
+
+folder = "youdownmp3"
+
+subprocess.call("clear", shell=True)
+subprocess.call(f"mkdir {folder}", shell=True)
 
 title = pyfiglet.figlet_format("!!YOUDOWN!!")
-print(title)
-print("by DERTY")
+print(f"{title}\nBY DERTY")
 print("<------------------------------------------------>")
+print(f"CREATED FOLDER {folder} WHERE MP3S WILL BE STORED IN")
+
+resp = input("will you be downloading multiple mp3s? (yes or no): ")
 
 def downmp3():
     vidurl = input("poot da YouTube video yoo-arr-el: ")
@@ -16,32 +25,16 @@ def downmp3():
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([vidinfo['webpage_url']])
     print("Da download complete now... {}".format(filename))
+    subprocess.call("mv {filename} {folder}", shell=True)
 
-def multiples():
-    vidurl = input("poot da YouTube video yoo-arr-el: ")
-    vidinfo = youtube_dl.YoutubeDL().extract_info(url = vidurl, download = False)
-    filename = f"{vidinfo['title']}.mp3"
-    options = {'format':'bestaudio/best',
-               'keepvideo':False,
-               'outtmpl':filename}
-    with youtube_dl.YoutubeDL(options) as ydl:
-        ydl.download([vidinfo['webpage_url']])
-    print("Da download complete now... {}".format(filename))
-    resp = input("continue? ('y' for yes, 'n' for no)")
-    if resp == "y":
-    continue
-    elif resp == "n":
-        break
 
-    multiples()
+def manymp3():
+    if resp == "yes":
+        downmp3()
+        resp2 = input("continue? (yes or no): ")
+        if resp2 == "yes":
+            print("great more yoo-arr-els\n continuing...")
+            downmp3()
 
-resp = input("multiples or single you-arr-els? ('m' or 's'): ")
 
-if resp == "m":
-    multiples()
-elif resp == "s":
-    downmp3()
-    break
-else:
-    print("invalid, choose a valid option")
-
+manymp3()
